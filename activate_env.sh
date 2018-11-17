da5ccb90b4c848699554926ed8c8b492950a7de7
wagnera <wagnera@vt.edu>
2018-11-17 16:45:53 -0500

    add bootstrap stuff for offline

--- activate_env.sh
+++ activate_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source bfenv/bin/activate
+source ~/bigfoot_webserver/bfenv/bin/activate
---
+++ start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ~/bigfoot_webserver/bfenv/bin/activate
+python3 ~/bigfoot_webserver/manage.py runserver 0.0.0.0:8000
+
+
